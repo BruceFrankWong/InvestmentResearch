a99724142358c55b28f18cdcb666bcb836ba113a
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-06-21 06:42:38 +0800

    Add models about futures and a test, not finished, temporary commit.

---
+++ src/database/model/futures.py
@@ -0,0 +1,85 @@
+# -*- coding: UTF-8 -*-
+
+__author__ = 'Bruce Frank Wong'
+
+
+from peewee import (
+    Model,
+    AutoField,
+    CharField,
+    FixedCharField,
+    DateField,
+    BooleanField,
+    IntegerField,
+    ForeignKeyField,
+)
+
+from .. import db
+from .exchange import Exchange
+
+
+class FuturesProduct(Model):
+    """
+    Futures product.
+    """
+    id = AutoField(primary_key=True)
+    symbol = FixedCharField(verbose_name='品种代码', max_length=2, unique=True)
+    name_zh = CharField(verbose_name='品种中文名称')
+    name_en = CharField(verbose_name='品种英文名称')
+    section = IntegerField(verbose_name='交易节数量')
+    optional_section = IntegerField(verbose_name='可选交易节序号')
+    exchange = ForeignKeyField(Exchange, backref='futures_list', on_delete='CASCADE')
+
+    class Meta:
+        database = db
+        table_name = 'futures_product'
+
+    def __repr__(self):
+        return f'<Futures(' \
+               f'symbol={self.symbol}, ' \
+               f'name_zh={self.name_zh}, ' \
+               f'name_en={self.name_en}, ' \
+               f'exchange={self.exchange}, ' \
+               f'alpha2={self.alpha2}, ' \
+               f'alpha3={self.alpha3}, ' \
+               f'numeric={self.numeric}' \
+               f')>'
+
+
+class FuturesContract(Model):
+    """
+    Futures contract.
+    """
+    id = AutoField(primary_key=True)
+    product = ForeignKeyField(FuturesProduct, backref='contract_list', on_delete='CASCADE')
+    symbol = FixedCharField(verbose_name='代码', max_length=4)
+    listing_date = DateField(verbose_name='上市日期')
+    expiration_date = DateField(verbose_name='上市日期')
+    delivery_date_begin = DateField(verbose_name='交割开始日期')
+    delivery_date_end = DateField(verbose_name='交割结束日期')
+
+    class Meta:
+        database = db
+        table_name = 'futures_contract'
+
+
+class FuturesTransactionRule(Model):
+    """
+    交易细节。
+    """
+    id = AutoField(primary_key=True)
+    date = DateField(verbose_name='日期')
+    product = ForeignKeyField(FuturesProduct, verbose_name='品种id', backref='transaction_list')
+    contract = ForeignKeyField(FuturesContract, verbose_name='合约id', backref='transaction_list')
+    margin_speculation_long = IntegerField(verbose_name='投机多单保证金率')      # margin = 1 means margin = 1%
+    margin_speculation_short = IntegerField(verbose_name='投机空单保证金率')      # margin = 1 means margin = 1%
+    margin_hedging_long = IntegerField(verbose_name='套保多单保证金率')      # margin = 1 means margin = 1%
+    margin_hedging_short = IntegerField(verbose_name='套保空单保证金率')      # margin = 1 means margin = 1%
+    limit_up = IntegerField(verbose_name='涨停板')      # margin = 1 means margin = 1%
+    limit_down = IntegerField(verbose_name='涨停板')      # margin = 1 means margin = 1%
+    is_fixed_transaction_fee = BooleanField(verbose_name='是否固定手续费')
+    transaction_fee = IntegerField(verbose_name='手续费')
+
+    class Meta:
+        database = db
+        table_name = 'futures_transaction_rule'
---
+++ tests/test_futures.py
@@ -0,0 +1,106 @@
+# -*- coding: UTF-8 -*-
+
+__author__ = 'Bruce Frank Wong'
+
+
+import pytest
+
+from typing import Any, Dict, List
+import datetime as dt
+
+from src.database import db, Exchange
+from src.database.model.futures import FuturesProduct, FuturesContract    # , TransactionRule
+
+
+@pytest.mark.run(order=8)
+def test_product():
+    """
+    Test for simulation.
+    """
+    # Futures.
+
+    # Create models but not commit into database with simulation data.
+    product_data_list: List[Dict[str, Any]] = [
+        {
+            'symbol': 'HC',
+            'name_zh': '热轧卷板',
+            'name_en': 'Hot rolled coils',
+            'exchange': 'SHFE'
+        },
+        {
+            'symbol': 'RB',
+            'name_zh': '螺纹钢',
+            'name_en': 'Steel Rebar',
+            'exchange': 'SHFE'
+        },
+    ]
+
+    product_list: List[FuturesProduct] = [
+        FuturesProduct(
+            symbol=data['symbol'],
+            name_zh=data['name_zh'],
+            name_en=data['name_en'],
+            exchange=Exchange.get(Exchange.symbol == data['exchange'])
+        ) for data in product_data_list
+    ]
+
+    # Commit into database.
+    if FuturesProduct.table_exists():
+        FuturesProduct.drop_table()
+    with db.atomic():
+        FuturesProduct.create_table()
+        FuturesProduct.bulk_create(product_list, batch_size=100)
+
+
+@pytest.mark.run(order=8)
+def test_contract():
+    """
+    Test for simulation.
+    """
+    # Contract.
+    contract_data_list: List[Dict[str, Any]] = [
+        {
+            'product': 'HC',
+            'symbol': '2107',
+            'listing_date': '2020-07-16',
+            'expiration_date': '2021-07-15',
+            'delivery_date_begin': '2021-07-16',
+            'delivery_date_end': '2021-07-20',
+        },
+        {
+            'product': 'HC',
+            'symbol': '2108',
+            'listing_date': '2020-08-18',
+            'expiration_date': '2021-08-16',
+            'delivery_date_begin': '2021-08-17',
+            'delivery_date_end': '2021-08-19',
+        },
+        {
+            'product': 'HC',
+            'symbol': '2109',
+            'listing_date': '2020-09-16',
+            'expiration_date': '2021-09-15',
+            'delivery_date_begin': '2021-09-16',
+            'delivery_date_end': '2021-09-22',
+        },
+    ]
+
+    contract_list: List[FuturesContract] = [
+        FuturesContract(
+            product=FuturesProduct.get(FuturesProduct.symbol == data['product']),
+            symbol=data['symbol'],
+            listing_date=dt.date.fromisoformat(data['listing_date']),
+            expiration_date=dt.date.fromisoformat(data['expiration_date']),
+            delivery_date_begin=dt.date.fromisoformat(data['delivery_date_begin']),
+            delivery_date_end=dt.date.fromisoformat(data['delivery_date_end'])
+        ) for data in contract_data_list
+    ]
+
+    # TransactionRule.create_table()
+    if FuturesContract.table_exists():
+        FuturesContract.drop_table()
+    with db.atomic():
+        FuturesContract.create_table()
+        FuturesContract.bulk_create(contract_list, batch_size=100)
+
+    db.close()
