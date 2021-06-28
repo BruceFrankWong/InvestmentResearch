0afe7414c8379b43a5d3df3b9f950d12c90d7b39
Bruce Frank Wong <github+bruce.frank.wong@gmail.com>
2021-06-28 16:08:38 +0800

    Temporary commit.

---
+++ InvestmentResearch/collector/sse.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+
+__author__ = 'Bruce Frank Wong'
+
+
+from typing import Dict
+from enum import Enum
+import json
+
+import requests
+
+
+class StockType(Enum):
+    ListedOnMainBoardA = 1      # 已上市，主板A股
+    ListedOnMainBoardB = 2      # 已上市，主板B股
+    ListedOnStarBoardB = 8      # 已上市，科创板
+    Listing = 3         # 待上市
+    Paused = 4          # 暂停上市
+    Terminated = 5      # 终止上市
+
+
+def get_listed_stock_from_sse():
+    header: Dict[str, str] = {
+        'Accept': 'text/html,'
+                  'application/xhtml+xml,'
+                  'application/xml;q=0.9,'
+                  'image/avif,'
+                  'image/webp,'
+                  'image/apng,'
+                  '*/*;q=0.8,'
+                  'application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/91.0.4472.114 Safari/537.36',
+        # 'refHost': 'query.sse.com.cn',
+        'Referer': 'http://www.sse.com.cn/',
+    }
+
+    url: str = 'http://query.sse.com.cn/security/stock/getStockListData2.do' \
+               '?&jsonCallBack=jsonpCallback97956&isPagination=false&stockCode=' \
+               '&csrcCode=&areaName=&stockType={stock_type}&pageHelp.cacheSize=1&pageHelp.beginPage=1' \
+               '&pageHelp.pageSize=100&pageHelp.pageNo=1&_=1624790621466'
+    content: str
+    stock_type: StockType = StockType.Listing
+    session = requests.Session()
+    response = session.get(url.format(stock_type=1), headers=header)
+
+    if response.status_code == 200:
+        if 'Set-Cookie' not in response.headers.keys():
+            print('OK, 2')
+            response.encoding = 'utf-8'
+            content = response.text
+            print(content)
+            result = json.loads(content[19:-1])
+            print(type(result))
+            print(result)
+            for k, v in result.items():
+                print(k, ': ', v)
+            for item in result['result']:
+                print(item)
+
+
+if __name__ == '__main__':
+    get_listed_stock_from_sse()
--- InvestmentResearch/database/model/stock.py
+++ InvestmentResearch/database/model/stock.py
@@ -19,6 +19,18 @@ from .base import BasicModel
 from .exchange import Exchange
 
 
+class StockStatus(BasicModel):
+    """
+    Status of stocks.
+    上市
+    """
+    id = AutoField(primary_key=True)
+    status = FixedCharField(verbose_name='股票状态', max_length=6, unique=True)
+
+    def __repr__(self):
+        return f'<StockStatus(status={self.status})>'
+
+
 class Stock(BasicModel):
     """
     Stock.
@@ -29,6 +41,7 @@ class Stock(BasicModel):
     name = CharField(verbose_name='品种中文名称')
     listing_date = DateField(verbose_name='上市日期', null=True)
     initial_contract = CharField(verbose_name='首批上市合约', null=True)
+    terminated_dat = DateField(verbose_name='终止上市日期', null=True)
     announced_date = DateField(verbose_name='公告日期', null=True)
     announcement_url = CharField(verbose_name='公告URL', null=True)
 
