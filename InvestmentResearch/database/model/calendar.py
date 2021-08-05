39361db0bd90310170c58fe2bee22e035f585465
Bruce Frank Wong <bruce.frank.wong@gmail.com>
2021-08-06 04:36:14 +0800

    Rename <holiday.py> to <calendar.py>, add model <Calendar>, and fix max_length error on field <name> in model <Holiday>.

--- InvestmentResearch/database/model/calendar.py
+++ InvestmentResearch/database/model/calendar.py
@@ -3,16 +3,48 @@
 __author__ = 'Bruce Frank Wong'
 
 
+from enum import Enum, auto
+
 from peewee import (
     AutoField,
     CharField,
-    FixedCharField,
     DateField,
+    FixedCharField,
+    IntegerField,
     ForeignKeyField,
 )
 
 from .base import BasicModel
 from .country import Country
+
+
+class DayStatus(Enum):
+    TradingDay = auto()
+    Weekend = auto()
+    Holiday = auto()
+    TemporaryClose = auto()
+
+
+class Calendar(BasicModel):
+    """
+    Trading calendar.
+    """
+    id = AutoField()
+    date = DateField(verbose_name='日历日')
+    status = IntegerField(verbose_name='状态')
+    country = ForeignKeyField(Country, backref='calendar')
+
+    class Meta:
+        depends_on = [
+            Country,
+        ]
+
+    def __repr__(self):
+        return f'<Calendar(' \
+               f'country={self.country}' \
+               f'date={self.date}, ' \
+               f'status={self.status}, ' \
+               f')>'
 
 
 class Holiday(BasicModel):
@@ -22,7 +54,7 @@ class Holiday(BasicModel):
     id = AutoField()
     begin = DateField(verbose_name='开始日期')
     end = DateField(verbose_name='结束日期')
-    name = FixedCharField(verbose_name='节假日名称', max_length=3)
+    name = FixedCharField(verbose_name='节假日名称', max_length=16)
     url = CharField(verbose_name='发文URL', null=True)
     country = ForeignKeyField(Country, backref='holiday_list')
 
